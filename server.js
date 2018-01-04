dafe1c630ad05b72c49ed65dc6d33d3cad0c1d3e
Kyle Rogers <pawndepot@Kyle-Rogers-Utah.local>
2018-01-03 21:32:28 -0700

    changing weird.html to home.html and changing the root behavior of server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/', function(req, res){
-	res.send('Hello World!');
+	res.sendFile('home.html', {root: 'public'});
 });
 
 app.listen(process.env.PORT || 3000, function(){
